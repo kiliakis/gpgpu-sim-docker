510376c8223ed9b2c06147575767c8a7f1417380
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2021-05-21 16:19:00 +0300

    minor updates

--- 1-create.sh
+++ 1-create.sh
@@ -1,4 +1,4 @@
 docker create --privileged --cpus 40 --rm \
-	-v "$(pwd)/results:/home/kiliakis/simulations-gpgpu/util/job_launching/results" \
+	-v "$(pwd)/results:/home/kiliakis/results" \
     --hostname docker \
 	--name gpgpusim-vm-1.0 kiliakis/gpgpusim-vm:1.0
